c4a772e8f60e6e3af4bae3ad73168628235d4855
haruk <haruki.tf.9529@gmail.com>
2025-09-28 02:11:25 +0900

    20250928

--- ManupulationManager.cs
+++ ManupulationManager.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace BudgetBook
 {
-    internal class ManupulationManager
+    internal static class ManupulationManager
     {
-        
+        public static void AddIncome()
+        {
+            Console.WriteLine("収入を追加します。");
+            // 収入追加の処理をここに記述
+        }
+        public static void AddExpense()
+        {
+            Console.WriteLine("支出を追加します。");
+            // 支出追加の処理をここに記述
+        }
+        public static void DisplayBalance()
+        {
+            Console.WriteLine("収支を表示します。");
+            // 収支表示の処理をここに記述
+        }
+        public static void deleteData()
+        {
+
+        }
+
+
+        public static void ExportToCSV()
+        {
+            Console.WriteLine("CSV出力を行います。");
+            // CSV出力の処理をここに記述
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             while (chooseResult != "6")
             {
                 MenuManager.ShowMenu();
-                chooseResult = Console.ReadLine();
+                chooseResult = Console.ReadLine() ?? "0";
 
 
                 switch (chooseResult)
@@ -16,22 +16,27 @@
                     case "1":
                         Console.WriteLine("収入を追加します。");
                         // 収入追加の処理をここに記述
+                        ManupulationManager.AddIncome();
                         break;
                     case "2":
                         Console.WriteLine("支出を追加します。");
                         // 支出追加の処理をここに記述
+                        ManupulationManager.AddExpense();
                         break;
                     case "3":
                         Console.WriteLine("収支を表示します。");
                         // 収支表示の処理をここに記述
+                        ManupulationManager.DisplayBalance();
                         break;
                     case "4":
                         Console.WriteLine("登録した操作を削除します。");
                         // 登録した操作を削除の処理をここに記述
+                        ManupulationManager.deleteData();
                         break;
                     case "5":
                         Console.WriteLine("CSV出力を行います。");
                         // CSV出力の処理をここに記述
+                        ManupulationManager.ExportToCSV();
                         break;
                     case "6":
                         Console.WriteLine("終了します。");
